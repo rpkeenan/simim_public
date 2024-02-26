9e260a4eb8f9899bccf84103a0d072950b788408
Ryan Keenan <rpkeenan@arizona.edu>
2024-02-26 23:45:25 +0100

    directly importing the downloaders for the sims instead of the modules

--- simim/siminterface/__init__.py
+++ simim/siminterface/__init__.py
@@ -1,4 +1,4 @@
 # Pre-load modules
-from . import illustris
-from . import universemachine
+from .illustris import illustris_catalogs
+from .universemachine import universemachine_catalogs
 # from . import simhandler
