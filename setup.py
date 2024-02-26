36fd21e44e3d602e39f6fc3b46e28d9c896c7b6d
Ryan Keenan <rpkeenan@arizona.edu>
2024-02-26 12:23:06 +0100

    setup stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='simim',
+      version='2.0',
+      description='Code for simulating the radio-submillimeter sky',
+      url='https://github.com/rpkeenan/simim',
+      author='R P Keenan',
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={'':'simim/resources/*.txt'}
+      )
