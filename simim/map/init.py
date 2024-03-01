aa5d00058f6eeaf20f6a7ed2ecd042634ffc91ce
Ryan Keenan <rpkeenan@arizona.edu>
2024-03-01 14:57:36 +0100

    Integrating and documenting map. Have a few functions left to do

---
+++ simim/map/init.py
@@ -0,0 +1,7 @@
+from .gridder import gridder
+from .gridder import load_grid
+from .gridder import gridder_function
+from .gridder import psf
+from .gridder import spectralpsf
+from .gridder import grid_from_axes
+from .gridder import grid_from_axes_and_function
