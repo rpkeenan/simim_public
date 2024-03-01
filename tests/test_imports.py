5ae4a7ef4d89f4c1c5a18ebdaa906bed8192e5c8
Ryan Keenan <rpkeenan@arizona.edu>
2024-03-01 14:59:43 +0100

    adding check for galprops and map module imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -4,7 +4,7 @@ def test_import_simim():
     import simim
 
     resources = dir(simim)
-    for check_resource in ['setupsimim','siminterface']:
+    for check_resource in ['setupsimim','siminterface','galprops','map']:
         assert check_resource in resources
 
 def test_import_siminterface():
@@ -16,3 +16,20 @@ def test_import_siminterface():
     for check_resource in ['IllustrisCatalogs','UniversemachineCatalogs','SnapHandler','SimHandler']:
         assert check_resource in resources
 
+def test_import_galprops():
+    """Make sure the expected files are imported - simim.galprops"""
+
+    import simim.galprops
+
+    resources = dir(simim.galprops)
+    for check_resource in ['Prop','MultiProp','prop_behroozi_sfr','prop_li_co']:
+        assert check_resource in resources
+
+def test_import_map():
+    """Make sure the expected files are imported - simim.map"""
+
+    import simim.map
+
+    resources = dir(simim.map)
+    for check_resource in ['Grid','Gridder','LoadGrid','GridFromAxes','GridFromAxesAndFunction','PSF','SpectralPSF','gridder_function',]:
+        assert check_resource in resources
