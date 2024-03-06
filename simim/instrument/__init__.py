599416730f25487b3f3210c5334fb987a84cb027
Ryan Keenan <rpkeenan@arizona.edu>
2024-03-07 00:12:18 +0100

    renaming builtins to match usage in Instrument class

--- simim/instrument/__init__.py
+++ simim/instrument/__init__.py
@@ -1,5 +1,4 @@
-# from simim.instrument.instrument import *
-# from simim.instrument.noise import *
-# from simim.instrument.psf import *
-# from simim.instrument.spec_response import *
 from .instrument import Instrument
+from .spec_response import *
+from .psf import *
+from .noise import *
