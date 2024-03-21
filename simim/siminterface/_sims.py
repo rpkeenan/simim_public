84391130c0cae2e804c524f49e85859ad6ad8a17
Ryan Keenan <rpkeenan@arizona.edu>
2024-03-21 15:32:52 +0100

    make identification of illustris sims more programatic

--- simim/siminterface/_sims.py
+++ simim/siminterface/_sims.py
@@ -10,7 +10,10 @@ _acceptedsims = ['Illustris-1','Illustris-2','Illustris-3',
                  'TNG300-1-Dark','TNG300-2-Dark','TNG300-3-Dark',
                  'TNG100-1','TNG100-2','TNG100-3',
                  'TNG100-1-Dark','TNG100-2-Dark','TNG100-3-Dark',
-                 'UniverseMachine-BolshoiPlanck','UniverseMachine-SMDPL','UniverseMachine-MDPL2']
+                 'UniverseMachine-BolshoiPlanck','UniverseMachine-SMDPL','UniverseMachine-MDPL2',
+                 '_testbox']
+_illsims = ['Illustris-1','Illustris-2','Illustris-3','Illustris-1-Dark','Illustris-2-Dark','Illustris-3-Dark']
+_tngsims = ['TNG300-1','TNG300-2','TNG300-3','TNG300-1-Dark','TNG300-2-Dark','TNG300-3-Dark','TNG100-1','TNG100-2','TNG100-3','TNG100-1-Dark','TNG100-2-Dark','TNG100-3-Dark']
 
 def _checksim(sim):
     """Check if a specified simulation name is acceptable"""
